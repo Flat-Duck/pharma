d0e3e78187aa14d265c39fce3cd87ea40cb66f0e
A.Mahidwei <xile.meko@gmail.com>
2024-07-01 04:03:30 +0200

    fix

--- resources/views/components/random-ad.blade.php
+++ resources/views/components/random-ad.blade.php
@@ -20,7 +20,7 @@
                                 {{ $ad->body }}
                             </p>
                         </div>
-                        <img src="{{ asset('assets/media/books/group-img.png') }}" alt="" class="main-img">
+                        <img src="{{ asset('storage'.$ad->image) }}" alt="" class="main-img">
                     </div>
                 </div>
             @endforeach
