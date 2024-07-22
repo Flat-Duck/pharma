7e9673d97257d2f1c2a29a8fb861d3ed518b0950
A.Mahidwei <xile.meko@gmail.com>
2024-07-22 02:22:28 +0200

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,20 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Adding an admin user
-        $user = \App\Models\User::factory()
-            ->count(1)
-            ->create([
-                'email' => 'admin@admin.com',
-                'password' => \Hash::make('admin'),
-            ]);
+        // $user = \App\Models\User::factory()
+        //     ->count(1)
+        //     ->create([
+        //         'email' => 'admin@admin.com',
+        //         'password' => \Hash::make('admin'),
+        //     ]);
         $this->call(PermissionsSeeder::class);
-        $this->call(AdSeeder::class);
-        $this->call(BrandSeeder::class);
-        $this->call(CartSeeder::class);
-        $this->call(CategorySeeder::class);
-        $this->call(OrderSeeder::class);
-        $this->call(ProductSeeder::class);
-        $this->call(SupplierSeeder::class);
+        // $this->call(AdSeeder::class);
+        // $this->call(BrandSeeder::class);
+        // $this->call(CartSeeder::class);
+        // $this->call(CategorySeeder::class);
+        // $this->call(OrderSeeder::class);
+        // $this->call(ProductSeeder::class);
+        // $this->call(SupplierSeeder::class);
         $this->call(UserSeeder::class);
     }
 }
